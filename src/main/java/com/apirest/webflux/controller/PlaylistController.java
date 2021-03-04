7397f930496df220b6fe28a5b902d60f2396e980
Cássio Leodegário <cassio.leodegario@gmail.com>
2021-03-04 16:45:12 -0400

    feat: adding stream endpoint

---
+++ src/main/java/com/apirest/webflux/controller/PlaylistController.java
@@ -0,0 +1,48 @@
+package com.apirest.webflux.controller;
+
+import com.apirest.webflux.document.Playlist;
+import com.apirest.webflux.services.PlaylistService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@RestController
+public class PlaylistController {
+
+    @Autowired
+    PlaylistService service;
+
+    @GetMapping(value="/playlist")
+    public Flux<Playlist> getPlaylist(){
+        Flux<Playlist> flux = service.findAll();
+        return flux;
+    }
+
+    @GetMapping(value="/playlist/{id}")
+    public Mono<Playlist> getPlaylistId(@PathVariable String id){
+        return service.findById(id);
+    }
+
+    @PostMapping(value="/playlist")
+    public Mono<Playlist> save(@RequestBody  Playlist playlist){
+        return service.save(playlist);
+    }
+
+
+    @GetMapping(value="/playlist/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Playlist>> getPlaylistByEvents(){
+
+        System.out.println("---Start get Playlists by WEBFLUX--- " + LocalDateTime.now());
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Playlist> events = service.findAll();
+        System.out.println("Passou aqui events");
+        return Flux.zip(interval, events);
+
+    }
+}
